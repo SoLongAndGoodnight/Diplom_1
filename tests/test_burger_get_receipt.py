81be87389dfd50eac89b4906496a24bb1af42e75
Elizaveta Korotkova <kavasima1994@icloud.com>
2025-03-25 10:31:03 +0200

    add tests/test_burger_get_receipt.py

---
+++ tests/test_burger_get_receipt.py
@@ -0,0 +1,19 @@
+import allure
+
+
+@allure.feature("Burger")
+class TestBurgerGetReceipt:
+    @allure.story("Расчёт цены бургера")
+    def test_get_receipt(self, burger, mock_bun, mock_ingredient1, mock_ingredient2):
+        with allure.step("Устанавливаем булочку"):
+            burger.set_buns(mock_bun)
+
+        with allure.step("Добавляем ингредиенты"):
+            burger.add_ingredient(mock_ingredient1)
+            burger.add_ingredient(mock_ingredient2)
+
+        with allure.step("Рассчитываем ожидаемую цену"):
+            expected_price = (100 * 2) + 200 + 50  # Две булочки + ингредиенты
+
+        with allure.step("Проверяем, что метод get_price() возвращает правильную цену"):
+            assert burger.get_price() == expected_price
